011f090dc949bbd1ed33fff6f1c10a21c53abfb0
Humaid <aburashid7788@gmail.com>
2024-12-31 12:01:58 +0400

    Add button and click event functionality

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ window.onload = function() {
     // Your code here
     alert("Welcome to the interactive web page!");
 };
+document.getElementById("clickMe").onclick = function() {
+    alert("You clicked the button!");
+};
+
